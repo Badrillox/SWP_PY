b459dc36140ea39cfb3a09dba885ad0385a56fc9
Badrillox <micperktold@tsn.at>
2022-03-17 08:53:44 +0100

    pushing project LinkedList_ArrayList to main branch

--- LinkedList_ArrayList/main.py
+++ LinkedList_ArrayList/main.py
@@ -11,7 +11,7 @@ def create_random_values(rlength=100):
     import random
     values = []
     for i in range(rlength):
-        values.append(random.randint(0, 10000)) #nrs range
+        values.append(random.randint(0, 10000))
     return values
 
 
@@ -31,47 +31,45 @@ def main():
     alist.menu()
 
 
-def rubneroutput(time, text, olist):
+def l_output(time, text, olist):
     print(" ")
     print(text + str(time))
     olist.output()
 
 
-def maintest(unn=1):
+def maintesting(unn=1):
     messungen = {}
     llist = LinkedList.LinkedList()
     alist = ArrayList.ArrayList()
     perf_time = time.perf_counter()
     val = create_random_values(10000)
     perf_time = time.perf_counter() - perf_time
-
-
+    print("Basic Values has been created! Performance Time:" + str(perf_time))
+    # Llist add
     perf_time = time.perf_counter()
     llist.adding_random(val)
     perf_time = time.perf_counter() - perf_time
-
+    l_output(perf_time, "Linked-list [Element ADD] with Performance Time: ", llist)
     messungen["llistadd"] = perf_time
-
+    # Alist add
     perf_time = time.perf_counter()
     alist.adding_elem(val)
     perf_time = time.perf_counter() - perf_time
-
+    l_output(perf_time, "Array-list [Element ADD] with Performance Time: ", alist)
     messungen["alistadd"] = perf_time
     # Llist sort
     perf_time = time.perf_counter()
     llist.insertionSortAsc()
     perf_time = time.perf_counter() - perf_time
-
+    l_output(perf_time, "Linked-list [Sort] with Performance Time: ", llist)
     messungen["llistsort"] = perf_time
-
+    # Alist sort
     perf_time = time.perf_counter()
     alist.sortAsc()
     perf_time = time.perf_counter() - perf_time
-
+    l_output(perf_time, "Array-list [Sort] with Performance Time: ", alist)
     messungen["alistsort"] = perf_time
     return messungen
-
-
 
 def testing():
     max_work = os.cpu_count()
@@ -82,13 +80,16 @@ def testing():
     max_work = 7 if max_work > 7 else max_work
     perftime= time.perf_counter()
     with multiproc.ProcessPoolExecutor(max_workers=max_work) as executor:
-        for messung in executor.map(maintest, arr_size):
+        for messung in executor.map(maintesting, arr_size):
            tester.append(messung)
     perftime = time.perf_counter()-perftime
     print(tester)
     print("performance time:"+str(perftime))\
+   #  tester = []
+   #  for i in range(1000):
+   #      tester.append(rubnertest_foo())
 
 
 
 if __name__ == '__main__':
-    print(maintest())
+    print(maintesting())
